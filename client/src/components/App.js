8408f2fa43e5d1d86b9f80b7b49bd2ea201fe0ea
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-07-25 23:21:51 +0300

    Create get all recipes apollo query

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Query } from 'react-apollo';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      Home
-      </div>
-    );
-  }
-}
+import './App.css';
+import { GET_ALL_RECIPES } from '../queries/index';
+
+const App = () => (
+  <div className="App">
+    <h1>Home</h1>
+    <Query query={ GET_ALL_RECIPES }>
+      { ({data, loading, error}) => {
+          if (loading) return <div>Loading</div>
+          if (error) return <div>Error</div>
+          console.log(data)
+          return (
+            <p>Recipes</p>
+          )
+        } }
+    </Query>
+  </div>
+)
 
 export default App;
