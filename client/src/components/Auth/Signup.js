c5be3d3638b1fd80086af7496674d619d4df480a
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-07-26 02:54:52 +0300

    Add logic to run sign up mutation on form submit

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
-
+import { SIGNUP_USER } from '../../queries'
 class Signup extends React.Component {
     state = {
         username: '',
@@ -16,17 +16,22 @@ class Signup extends React.Component {
         });
     };
 
+    handleSubmit = (event, signupUser) => {
+        event.preventDefault();
+        signupUser().then(data => {
+            console.log(data)
+        })
+    }
+
     render() {
         const {username, email, password, passwordConfirmation} = this.state;
         return (
             <div className="App">
               <h2 className="App">Signup</h2>
-              <Mutation mutation={ SIGNUP_USER }>
-                { (data, loading, error) => {
-                  
-                  
+              <Mutation mutation={ SIGNUP_USER } variables={ { username, email, password } }>
+                { (signupUser, {data, loading, error}) => {
                       return (
-                          <form className="form">
+                          <form className="form" onSubmit={ event => this.handleSubmit(event, signupUser) }>
                             <input type="text" name="username" placeholder="Username" onChange={ this.handleChange } value={ username } />
                             <input type="email" name="email" placeholder="Email Address" onChange={ this.handleChange } value={ email } />
                             <input type="password" name="password" placeholder="Password" onChange={ this.handleChange } value={ password } />
