3773b43158fd93b64a32e58c229b83680c970a22
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-07-26 01:27:05 +0300

    Add react router and create signin.js and signup.js component files

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
+import App from './components/App';
+import Signin from './components/Auth/Signin';
+import Signup from './components/Auth/Signup';
+
+import './index.css';
+
 
 const client = new ApolloClient({
     uri: 'http://localhost:3300/graphql'
 });
 
+const Root = () => (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={ App } />
+        <Route path='/signin' component={ Signin } />
+        <Route path='/signup' component={ Signup } />
+        <Redirect to="/" />
+      </Switch>
+    </Router>)
 
 
-ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider> , document.getElementById('root'));
+ReactDOM.render(<ApolloProvider client={ client }>
+                  <Root />
+                </ApolloProvider>, document.getElementById('root'));
 
