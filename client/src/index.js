fd67039648726550936994a6b6dc7f1b38487e3f
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-07-25 22:04:59 +0300

    Connect reac app to the server using apollo

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:3300/graphql'
+});
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider> , document.getElementById('root'));
 
