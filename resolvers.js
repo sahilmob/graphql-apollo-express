442201fcd74c3e2f17716c7a15c949e18dfb8e65
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-07-25 21:40:19 +0300

    Add logic to getAllRecipes Query

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,10 @@
 exports.resolvers = {
     Query: {
-        getAllRecipes: () => {}
+        getAllRecipes: async(root, args, {Recipe}) => {
+            const AllRecipes = await Recipe.find();
+            return AllRecipes;
+            // it could be "return await Recipe.find();"
+        }
     },
     Mutation: {
         addRecipe: async (root,{name, description, category, instructions, username}, {Recipe}) => {
