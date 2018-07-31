b5d98528cada578f1eaa40448b70e6412c2c436e
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-07-31 22:22:00 +0300

    Add Recipe page and get recipe id from the url

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ import withSession from './components/withSession';
 import Search from "./components/Recipe/Search";
 import AddRecipe from './components/Recipe/AddRecipe';
 import Profile from './components/Profile/Profile';
+import RecipePage from "./components/Recipe/RecipePage";
 import "./index.css";
 
 
@@ -44,6 +45,7 @@ const Root = ({refetch, session}) => (
         <Route path="/signin" render={ () => <Signin refetch={ refetch } /> } />
         <Route path="/signup" render={ () => <Signup refetch={ refetch } /> } />
         <Route path="/recipe/add" component={ AddRecipe } />
+        <Route path="/recipes/:_id" component={ RecipePage } />
         <Route path="/profile" component={ Profile } />
         <Redirect to="/" />
       </Switch>
