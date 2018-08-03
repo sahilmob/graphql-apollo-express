0106f8b9344a1cb803e5c68bb47906ffcbf46019
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-03 18:12:52 +0300

    Add logic to show the like button only to the logged in users

--- client/src/components/Recipe/RecipePage.js
+++ client/src/components/Recipe/RecipePage.js
@@ -2,41 +2,42 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Query } from 'react-apollo';
 import { GET_RECIPE } from '../../queries/index'
+import LikeRecipe from './LikeRecipe';
 const RecipePage = ({match}) => {
-    const {_id} = match.params;
-    return (
-        <Query query={ GET_RECIPE } variables={ { _id } }>
-          { ({data, loading, err}) => {
-                if (loading) {
-                    return <div>Loading</div>
-                }
-                if (err) {
-                    return <div>err</div>
-                }
-                return (
-                    <div className="App">
-                      <h2>{ data.getRecipe.name }</h2>
-                      <p>Category:
-                        { data.getRecipe.category }
-                      </p>
-                      <p>Description:
-                        { data.getRecipe.description }
-                      </p>
-                      <p>Instructions:
-                        { data.getRecipe.instructions }
-                      </p>
-                      <p>Likes:
-                        { data.getRecipe.likes }
-                      </p>
-                      <p>Create By:
-                        { data.getRecipe.username }
-                      </p>
-                      <button>Like</button>
-                    </div>
-                )
-            } }
-        </Query>
-    )
+  const {_id} = match.params;
+  return (
+    <Query query={ GET_RECIPE } variables={ { _id } }>
+      { ({data, loading, err}) => {
+          if (loading) {
+            return <div>Loading</div>
+          }
+          if (err) {
+            return <div>err</div>
+          }
+          return (
+            <div className="App">
+              <h2>{ data.getRecipe.name }</h2>
+              <p>Category:
+                { data.getRecipe.category }
+              </p>
+              <p>Description:
+                { data.getRecipe.description }
+              </p>
+              <p>Instructions:
+                { data.getRecipe.instructions }
+              </p>
+              <p>Likes:
+                { data.getRecipe.likes }
+              </p>
+              <p>Create By:
+                { data.getRecipe.username }
+              </p>
+              <LikeRecipe />
+            </div>
+          )
+        } }
+    </Query>
+  )
 }
 
 export default withRouter(RecipePage);
